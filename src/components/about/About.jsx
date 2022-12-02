f363c620ff8fb30ca9cd6d34f12a8922eed04090
yujiho1910 <yujiho54@gmail.com>
2022-12-02 10:17:10 +0800

    configured for react deployment

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="about image moosemoose" />
+            <img src={ME} alt="about moosemoose" />
           </div>
         </div>
 
